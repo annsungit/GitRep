1831c6cf763d08e61211ddec8d39979888d9feba
annsungit <ann.sunwx@gmail.com>
2021-01-06 23:26:18 -0500

    first commit

---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	// Testng xml file from Maven
+	//How to execute all test cases from test folder- mvn Test
+	
+	@Test 
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void elementsUi()
+	{
+		System.out.println("elementsUi");
+	}
+}
